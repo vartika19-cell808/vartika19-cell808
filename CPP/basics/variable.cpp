c96bc2d8e2610f7581dcdca3fafbe261acffeb0a
vartika19-cell808 <vartikakeshari2113@gmail.com>
2026-02-05 01:26:58 +0530

    Create variable.cpp

---
+++ CPP/basics/variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 20;
+    float marks = 89.5;
+    char grade = 'A';
+
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Grade: " << grade;
+
+    return 0;
+}
