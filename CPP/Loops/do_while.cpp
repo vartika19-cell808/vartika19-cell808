d013dc792672ac42921803122b887dfc1e27426d
vartika19-cell808 <vartikakeshari2113@gmail.com>
2026-02-05 01:34:18 +0530

    Create do_while.cpp

---
+++ CPP/Loops/do_while.cpp
@@ -0,0 +1,14 @@
+//print number from 1-5
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    do {
+        cout << i << " ";
+        i++;
+    } while(i <= 5);
+
+    return 0;
+}
