1c138ee5fff08a43555c228e3ab2417733e1ee0d
vartika19-cell808 <vartikakeshari2113@gmail.com>
2026-02-05 01:44:43 +0530

    Create for_loop_sum.cpp

---
+++ CPP/Loops/for_loop_sum.cpp
@@ -0,0 +1,18 @@
+//calculate the sum of first N natural number 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << "Sum = " << sum;
+
+    return 0;
+}
