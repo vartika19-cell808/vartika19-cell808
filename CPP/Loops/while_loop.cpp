b80e4358679d61688df4932f7e3756012f1ec412
vartika19-cell808 <vartikakeshari2113@gmail.com>
2026-02-05 01:32:08 +0530

    Create while_loop.cpp

---
+++ CPP/Loops/while_loop.cpp
@@ -0,0 +1,14 @@
+//print the number from 1-5
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    while(i <= 5) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
